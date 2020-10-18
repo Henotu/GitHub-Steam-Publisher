f827e0af31155792123b5adfda43ab85ac6ab463
Henotu <Henrik.Kayser@freenet.de>
2020-10-18 12:55:39 +0200

    Bugfixes in main.py
    * Changes would only show the first word, because of missing quotation marks
    * Addons would publish even if the addon is up-to date with the .git

--- main.py
+++ main.py
@@ -34,43 +34,43 @@ def main():
                 else:
                     subprocess.call("git -C ./Repositories/ clone https://github.com/{}.git".format(data[1]))
                 addons[addon][2] = date
-            #
-            #Publish the addon to the workshop
-            #
-            copytree("./Repositories/" + repoName, "./temp/" + repoName, ignore=ignore_patterns(".git", "*.md")) #filter for different files
-            #Create addon.json file
-            db = {}
-            addonType = {0: "ServerContent", 1: "gamemode", 2: "map", 3: "weapon", 4: "vehicle", 5: "npc", 6: "tool", 7: "effects", 8: "model"}
-            addonTag = {0: "fun", 1: "roleplay", 2: "scenic", 3: "movie", 4: "realism", 5: "cartoon", 6: "water", 7: "comic", 8: "build"}
-            title = data[0]
-            title = sub(r"\"\b", "", title)
-            title = sub(r"\b\"", "", title)
-            db["title"] = title
-            for name in addonType:
-                print("[{}] {}".format(name, addonType[name]))
-            typeInput = input("\nPlease choose your type of addon for {} (0-8): ".format(data[0]))
-            for name in addonTag:
-                print("[{}] {}".format(name, addonTag[name]))
-            tagInput = input("\nPlease choose up to two tags (00-88): ")
-            tagArray = []
-            for char in Split(tagInput):
-                tagArray.append(addonTag[int(char)])
-            try:
-                db["type"] = addonType[int(typeInput)]
-                db["tags"] = tagArray
-            except:
-                db["type"] = "tool"
-                db["tags"] = ["fun", "roleplay"]
-            with open("./temp/{}/addon.json".format(repoName), "w+") as addonInfo:
-                json.dump(db, addonInfo)
-            #Create .gma file
-            subprocess.call("{} create -folder {}/temp/{} -out {}/temp/{}.gma".format(gmad, dirPath, repoName, dirPath, repoName))
-            # Publish .gma file
-            changeInput = input("What are the changes of {}?:\n".format(data[0]))
-            subprocess.call("{} update -id {} -addon {}/temp/{}.gma -changes {}".format(gmpublish, addon, dirPath, repoName, changeInput))
-            # Delete temp files
-            rmtree("./temp/" + repoName)
-            remove("./temp/{}.gma".format(repoName))
+                #
+                #Publish the addon to the workshop, when out of date
+                #
+                copytree("./Repositories/" + repoName, "./temp/" + repoName, ignore=ignore_patterns(".git", "*.md")) #filter for different files
+                #Create addon.json file
+                db = {}
+                addonType = {0: "ServerContent", 1: "gamemode", 2: "map", 3: "weapon", 4: "vehicle", 5: "npc", 6: "tool", 7: "effects", 8: "model"}
+                addonTag = {0: "fun", 1: "roleplay", 2: "scenic", 3: "movie", 4: "realism", 5: "cartoon", 6: "water", 7: "comic", 8: "build"}
+                title = data[0]
+                title = sub(r"\"\b", "", title)
+                title = sub(r"\b\"", "", title)
+                db["title"] = title
+                for name in addonType:
+                    print("[{}] {}".format(name, addonType[name]))
+                typeInput = input("\nPlease choose your type of addon for {} (0-8): ".format(data[0]))
+                for name in addonTag:
+                    print("[{}] {}".format(name, addonTag[name]))
+                tagInput = input("\nPlease choose up to two tags (00-88): ")
+                tagArray = []
+                for char in Split(tagInput):
+                    tagArray.append(addonTag[int(char)])
+                try:
+                    db["type"] = addonType[int(typeInput)]
+                    db["tags"] = tagArray
+                except:
+                    db["type"] = "tool"
+                    db["tags"] = ["fun", "roleplay"]
+                with open("./temp/{}/addon.json".format(repoName), "w+") as addonInfo:
+                    json.dump(db, addonInfo)
+                #Create .gma file
+                subprocess.call("{} create -folder {}/temp/{} -out {}/temp/{}.gma".format(gmad, dirPath, repoName, dirPath, repoName))
+                # Publish .gma file
+                changeInput = input("What are the changes of {}?:\n".format(data[0]))
+                subprocess.call("{} update -id {} -addon {}/temp/{}.gma -changes \"{}\"".format(gmpublish, addon, dirPath, repoName, changeInput))
+                # Delete temp files
+                rmtree("./temp/" + repoName)
+                remove("./temp/{}.gma".format(repoName))
         # update addons.json
         with open("./addons.json", "w") as addonsFile:
             json.dump(addons, addonsFile)
